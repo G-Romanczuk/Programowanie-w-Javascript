ef779387d34ecdf0af5d1083c6514175bb6aa9ba
G-Romanczuk <gxbiromanczuk@gmail.com>
2022-11-13 22:40:48 +0100

    lab 2 middle version

--- lab1/main.js
+++ lab1/main.js
@@ -1,54 +1,54 @@
-function onLoad(){
-
-    document.querySelectorAll('.form').forEach(x => {
-        x.addEventListener('change', () => {calculate()}
-        )});
+function onLoad() {
+  document.querySelectorAll(".form").forEach((x) => {
+    x.addEventListener("change", () => {
+      calculate();
+    });
+  });
 }
 
 function calculate() {
-        var list = document.querySelectorAll(".form");
-        var array = [];
-        for(var i = 0; i< list.length; i++)
-        {
-            array[i] = parseInt(list[i].value)
-        }
-        //SUMA
-        var sum = 0
-        list.forEach(x=>sum+= parseInt(x.value));
-        document.getElementById('sum').innerHTML = sum; 
+  var list = document.querySelectorAll(".form");
+  var array = [];
+  for (var i = 0; i < list.length; i++) {
+    array[i] = parseInt(list[i].value);
+  }
+  //SUMA
+  var sum = 0;
+  list.forEach((x) => (sum += parseInt(x.value)));
+  document.getElementById("sum").innerHTML = sum;
 
-        //ŚREDNIA
-        var avg = 0
-        var count = 0
-        list.forEach(x=>count++);
-        avg = sum/count;
-        document.getElementById('avg').innerHTML = avg; 
+  //ŚREDNIA
+  var avg = 0;
+  var count = 0;
+  list.forEach((x) => count++);
+  avg = sum / count;
+  document.getElementById("avg").innerHTML = avg;
 
-        //MINIMUM
-        var min = Math.min(...array)
-        document.getElementById('min').innerHTML = min; 
-        //MAKSIMUM
-        var max = Math.max(...array)
-        document.getElementById('max').innerHTML = max; 
+  //MINIMUM
+  var min = Math.min(...array);
+  document.getElementById("min").innerHTML = min;
+  //MAKSIMUM
+  var max = Math.max(...array);
+  document.getElementById("max").innerHTML = max;
 }
 
 var countArea = 3;
 
 async function Add() {
- countArea++;
- var area = document.createElement("input");
-                area.setAttribute("type", "number");
-                area.setAttribute("id", countArea);
-                area.setAttribute("value", "1");
-                area.setAttribute("class", "form");
-document.getElementById("formContainer").appendChild(area);  
-onLoad();   
-calculate();     
+  countArea++;
+  var area = document.createElement("input");
+  area.setAttribute("type", "number");
+  area.setAttribute("id", countArea);
+  area.setAttribute("value", "1");
+  area.setAttribute("class", "form");
+  document.getElementById("formContainer").appendChild(area);
+  onLoad();
+  calculate();
 }
-   
+
 function Delete() {
-document.getElementById( countArea ).remove()
-countArea--;
-onLoad();
-calculate();
+  document.getElementById(countArea).remove();
+  countArea--;
+  onLoad();
+  calculate();
 }
--- lab2/main.js
+++ lab2/main.js
@@ -1,22 +1,92 @@
-let dots          = 4;
-let sliderElem    = document.querySelector('.slider')
-let dotElems      = sliderElem.querySelectorAll('.slider__dot')
-let indicatorElem = sliderElem.querySelector('.slider__indicator')
+var timeout;
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+// current slide counter
+let curSlide = -1;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+Next();
 
-Array.prototype.forEach.call(dotElems, (dotElem) => {
-		
-	dotElem.addEventListener('click', (e) => {
+// loop through slides and set each slides translateX property to index * 100%
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
 
-		let currentPos = parseInt(sliderElem.getAttribute('data-pos'))
-		let newPos     = parseInt(dotElem.getAttribute('data-pos'))
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
 
-		let newDirection     = (newPos > currentPos ? 'right' : 'left')
-		let currentDirection = (newPos < currentPos ? 'right' : 'left')
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", Next);
+function Next() {
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
 
-		indicatorElem.classList.remove(`slider__indicator--${ currentDirection }`)
-		indicatorElem.classList.add(`slider__indicator--${ newDirection }`)		
-		sliderElem.setAttribute('data-pos', newPos)
-		
-	})
-	
-})
+  //   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+
+  timeout = setTimeout(Next, 2000);
+}
+
+// select prev slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+// select dots
+const dot1 = document.querySelector(".dot1").addEventListener("click", () => {
+  curSlide = 0;
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+const dot2 = document.querySelector(".dot2").addEventListener("click", () => {
+  curSlide = 1;
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+const dot3 = document.querySelector(".dot3").addEventListener("click", () => {
+  curSlide = 2;
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+const dot4 = document.querySelector(".dot4").addEventListener("click", () => {
+  curSlide = 3;
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+//pausing the timer
+const pause = document.querySelector("#pause");
+
+pause.addEventListener("click", function () {
+  clearTimeout(timeout);
+});
+
+//playing it again
+const play = document.querySelector("#play");
+
+play.addEventListener("click", function () {
+  timeout = setTimeout(Next, 2000);
+});
